aa4c16c46e0c1a7eb7cbea4f7d7920de71f8d9fe
Gabriel Pivato <pivatogabriel@gmail.com>
2018-06-15 01:08:20 -0300

    update

---
+++ experimento5/logic.py
@@ -0,0 +1,31 @@
+# livrarias necessarias
+import RPi.GPIO as GPIO
+import time
+import signal
+import sys
+
+# para poder usar os pinos
+GPIO.setmode(GPIO.BCM)
+
+# pino a ser usado
+button = 14
+
+# limpar a GPIO, caso preciso
+def limpa(signal, frame):
+    GPIO.cleanup()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, limpa)
+
+# define pino como saida
+GPIO.setup(button, GPIO.IN)
+
+while True:
+
+    if GPIO.input(button) == True:
+        print("Botao pressionado")
+
+    else:
+        print("Botao despressionado")
+
+    time.sleep(0.08)
