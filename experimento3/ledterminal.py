74d25939fd8c7601c24c95fbf1ac7f25c0755ad2
Gabriel Pivato <pivatogabriel@gmail.com>
2018-06-14 22:58:47 -0300

    update

--- experimento3/ledterminal.py
+++ experimento3/ledterminal.py
@@ -1,80 +1,88 @@
-# livrarias necessarias
+# modulos necessarios
 import RPi.GPIO as GPIO
 import time
 import signal
 import sys
 
-l = "L"
-d = "D"
+led1 = 14
+led2 = 15
+led3 = 18
 
 # para poder usar os pinos
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode( GPIO.BCM )
 
 # pinos a serem usados
-def checkpin():
+def checkpin( led, state ):
     
-    print("Deseja ligar ou desligar um led: (L/D)")
-    escolha = input()
-
-    if escolha == l: 
-
-        print("Digite o led a ser ligado: (1, 2, 3)")
-        led = input()
-
+    if True:
         if led == 1:
-            led = 14
-            GPIO.setup(led, GPIO.OUT)
-            return led, True 
+            if state == 1:
+                state = True
+                led = led1 
+                return led, state
+
+            elif state == 0:
+                state = False
+                led = led1
+                return led, state
+
+            else:
+                pass
 
         elif led == 2:
-            led = 15
-            GPIO.setup(led, GPIO.OUT)
-            return led, True 
+            if state == 1:
+                state = True
+                led = led2
+                return led, state
 
+            elif state == 0:
+                state = False
+                led = led2
+                return led, state
+
+            else:
+                pass
+            
         elif led == 3:
-            led = 18
-            GPIO.setup(led, GPIO.OUT)
-            return led, True 
+            if state == 1:
+                state = True
+                led = led3
+                return led, state
 
-        else:
-            checkpin()
+            elif state == 0:
+                state = False
+                led = led3
+                return led, state
 
-    elif escolha == d:
-
-        print("Digite o led a ser desligado: (1, 2, 3)")
-        led = input()
-
-        if led == 1:
-            led = 14
-            GPIO.setup(led, GPIO.OUT)
-            return led, False 
-
-        elif led == 2:
-            led = 15
-            GPIO.setup(led, GPIO.OUT)
-            return led, False 
-
-        elif led == 3:
-            led = 18
-            GPIO.setup(led, GPIO.OUT)
-            return led, False 
-
-        else:
-            checkpin()
-    
+            else:
+                pass
+         
     else:
-        checkpin()
+        pass
 
 # limpar a GPIO, caso preciso
-def limpa(signal, frame):
+def limpa( signal, frame ):
     GPIO.cleanup()
     sys.exit(0)
 
-signal.signal(signal.SIGINT, limpa)
+signal.signal( signal.SIGINT, limpa )
 
 # define pinos como saida
+GPIO.setup( led1, GPIO.OUT )
+GPIO.setup( led2, GPIO.OUT )
+GPIO.setup( led3, GPIO.OUT )
 
 while True:
 
-    GPIO.output(checkpin())
+    print("Digite o led, e se deseja ligar, ex:")
+    print("(1 1 || ligar led 1) ou (2 0 || desligar led 2)")
+    print("Um numero por linha!!!!")
+
+    led = input()
+    state = input()
+    
+    checkpin( led, state )
+    
+    GPIO.setup( led, GPIO.OUT )
+    GPIO.output( led, state )
 
